b6e84e5fada51564d54ffc59fd929adf5bc95682
AlexGrin1 <a_grynevich@mail.ru>
2021-03-17 22:42:42 +0300

    refactor: change script

---
+++ Virtual-Keyboard/script.js
@@ -0,0 +1,34 @@
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
+  properties: {
+    value: "",
+    capsLock: false,
+  },
+  init() {
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
+
+    this.elements.main.classList.add("keyboard", "keyboard--hidden");
+    this.elements.keysContainer, classList.add("keyboard__keys");
+    this.elements.keysContainer.appendChild(this._createKeys());
+
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+
+    document.querySelectorAll(".use-keyboard-input").forEach((element) => {
+      element.addEventListener("focus", () => {
+        this.open(element.value, (currentValue) => {
+          element.value = currentValue;
+        });
+      });
+    });
+  },
+};
